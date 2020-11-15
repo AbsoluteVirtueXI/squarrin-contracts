3b814d9700ea233c642d8bf1f726a07598f05013
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-15 23:36:31 +0100

    quadreum contract + test OK

--- contracts/Quadreum.sol
+++ contracts/Quadreum.sol
@@ -1,32 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Squarrin.sol";
 
-contract Quadreum is ERC777 {
+contract Quadreum is ERC777, Ownable {
     uint256 private constant _TOTAL_SUPPLY = 8 * 10**27;
     string private constant _NAME = "Quadreum";
     string private constant _SYMBOL = "QUAD";
-    address private _owner;
-    Squarrin private _squarrin;
 
-    constructor(address owner, address[] memory defaultOperators) public ERC777(_NAME, _SYMBOL, defaultOperators) {
-        _owner = owner;
-        // defaultOperators[0] is the Squarrin contract address
-        _squarrin = Squarrin(payable(defaultOperators[0]));
-        _onCreate();
+    modifier onlyOneDefaultOperator(address[] memory defaultOperators) {
+        require(defaultOperators.length == 1, "Quadreum: Only one default operator is allowed");
+        _;
     }
 
-    function _onCreate() internal virtual {
-        _mint(_owner, _TOTAL_SUPPLY, "", "");
-        _squarrin.setQuadreum();
+    constructor(address owner_, address[] memory defaultOperators)
+        public
+        onlyOneDefaultOperator(defaultOperators)
+        ERC777(_NAME, _SYMBOL, defaultOperators)
+    {
+        _onCreate(owner_);
     }
 
-    function getOwner() public view returns (address) {
-        return _owner;
+    function _onCreate(address owner_) internal virtual {
+        _register();
+        transferOwnership(owner_);
+        _mint(owner(), _TOTAL_SUPPLY, "", "");
     }
 
-    function getSquarrin() public view returns (address) {
-        return address(_squarrin);
+    function _register() private {
+        Squarrin squarrin = Squarrin(payable(defaultOperators()[0]));
+        squarrin.setQuadreum();
     }
 }
--- test/Quadreum_test.js
+++ test/Quadreum_test.js
@@ -1,48 +1,77 @@
 const { accounts, contract } = require('@openzeppelin/test-environment');
-const { BN, singletons } = require('@openzeppelin/test-helpers');
+const { BN, expectRevert, singletons } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
-
-const Quadreum = contract.fromArtifact('Quadreum');
 const Squarrin = contract.fromArtifact('Squarrin');
+const Quadreum = contract.fromArtifact('Quadreum');
 
 describe('Quadreum', () => {
-  const [owner, registryFunder] = accounts;
+  const [dev, owner, registryFunder, addr1, addr2] = accounts;
   const TOTAL_SUPPLY = new BN('8' + '0'.repeat(27));
   const NAME = 'Quadreum';
   const SYMBOL = 'QUAD';
-  beforeEach(async function () {
-    this.squarrin = await Squarrin.new(5, { from: owner });
-    const defaultOperators = [this.squarrin.address];
-    this.erc1820 = await singletons.ERC1820Registry(registryFunder);
-    this.quadreum = await Quadreum.new(owner, defaultOperators, { from: owner });
+
+  context('Quadreum without only 1 default operator', function () {
+    beforeEach(async function () {
+      this.squarrin = await Squarrin.new(5, { from: dev });
+      this.erc1820 = await singletons.ERC1820Registry(registryFunder);
+    });
+
+    it('Quadreum reverts if no default operators at deployment', async function () {
+      await expectRevert(Quadreum.new(owner, [], { from: dev }), 'Quadreum: Only one default operator is allowed');
+    });
+
+    it('Quadreum reverts if more than 1 default operators at deployment', async function () {
+      await expectRevert(
+        Quadreum.new(owner, [this.squarrin.address, addr1], { from: dev }),
+        'Quadreum: Only one default operator is allowed',
+      );
+    });
   });
 
-  it('has name Quadreum', async function () {
-    expect(await this.quadreum.name()).to.equal(NAME);
-  });
+  context('Quadreum with a successful deployment', function () {
+    beforeEach(async function () {
+      this.squarrin = await Squarrin.new(5, { from: dev });
+      this.erc1820 = await singletons.ERC1820Registry(registryFunder);
+      this.defaultOperators = [this.squarrin.address];
+      this.quadreum = await Quadreum.new(owner, this.defaultOperators, { from: dev });
+    });
 
-  it('has symbol QUAD', async function () {
-    expect(await this.quadreum.symbol()).to.equal(SYMBOL);
-  });
+    it(`has name ${NAME}`, async function () {
+      expect(await this.quadreum.name()).to.equal(NAME);
+    });
 
-  it('sends total supply to owner', async function () {
-    expect(await this.quadreum.balanceOf(owner)).to.be.bignumber.equal(TOTAL_SUPPLY);
-  });
+    it(`has symbol ${SYMBOL}`, async function () {
+      expect(await this.quadreum.symbol()).to.equal(SYMBOL);
+    });
 
-  it('has an owner', async function () {
-    expect(await this.quadreum.getOwner()).to.equal(owner);
-  });
+    it(`has total supply of ${TOTAL_SUPPLY.toString()}`, async function () {
+      expect(await this.quadreum.totalSupply()).to.be.a.bignumber.equal(TOTAL_SUPPLY);
+    });
 
-  it('has Squarrin address contract', async function () {
-    expect(await this.quadreum.getSquarrin()).to.equal(this.squarrin.address);
-  });
+    it('has Squarrin contract as only one default operator', async function () {
+      expect((await this.quadreum.defaultOperators()).length, 'There are more than 1 default operator').to.equal(1);
+      expect((await this.quadreum.defaultOperators())[0], 'Squarrin address is invalid').to.equal(
+        this.squarrin.address,
+      );
+    });
 
-  it('set Quadreum address in Squarrin contract', async function () {
-    expect(await this.squarrin.getQuadreum()).to.equal(this.quadreum.address);
-  });
+    it('has registered itself in Squarrin contract', async function () {
+      expect(await this.squarrin.getQuadreum()).to.equal(this.quadreum.address);
+    });
 
-  it('has Squarrin contract as only one default operator', async function () {
-    expect((await this.quadreum.defaultOperators()).length, 'There are more than 1 default operator').to.equal(1);
-    expect((await this.quadreum.defaultOperators())[0], 'Squarrin address is invalid').to.equal(this.squarrin.address);
+    it('has an owner', async function () {
+      expect(await this.quadreum.owner()).to.equal(owner);
+    });
+
+    it('sends total supply to owner', async function () {
+      expect(await this.quadreum.balanceOf(owner)).to.be.bignumber.equal(TOTAL_SUPPLY);
+    });
+
+    it('reverts on Squarrin if a second Quadreum contract is deployed', async function () {
+      await expectRevert(
+        Quadreum.new(owner, this.defaultOperators, { from: dev }),
+        'Squarrin: Quadreum address is already set',
+      );
+    });
   });
 });
